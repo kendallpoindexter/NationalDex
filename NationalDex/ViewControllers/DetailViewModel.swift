5b09590308959f72c82b14a62d49a1dac088fbca
kendall <kendall.poindexter@detroitlabs.com>
2020-05-19 15:11:17 -0400

    add method to append a pokemon's name and store it in the names array

--- NationalDex/ViewControllers/DetailViewModel.swift
+++ NationalDex/ViewControllers/DetailViewModel.swift
@@ -15,6 +15,7 @@ protocol DetailViewModelDelegate: AnyObject  {
 
 class DetailViewModel {
     private let networkManager = NetworkManager()
+    private let favoritesController = FavoritesController()
     let pokedexEntry: PokedexEntry
     var pokemon: Pokemon?
     private(set) var error: Error?
@@ -38,6 +39,20 @@ class DetailViewModel {
                 self.error? = error
             }
         }
+    }
+    
+//    func favoritesViewModel(with name: String) -> FavoritesViewModel {
+//        let viewModel = FavoritesViewModel(controller: favoritesController)
+//        return viewModel
+//    }
+    
+    func addFavoritePokemon(name: String) {
+        favoritesController.names.append(name)
+        print(favoritesController.names)
+    }
+    
+    func removeFavoritePokemon(name:String) {
+        favoritesController.removeObject(with: name)
     }
 }
 
