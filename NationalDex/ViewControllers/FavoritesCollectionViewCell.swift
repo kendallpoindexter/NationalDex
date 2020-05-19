efef0d6c8000a969b7b46e87de3b7415ccce6a87
kendall <kendall.poindexter@detroitlabs.com>
2020-05-19 15:11:55 -0400

    Create FavoritesCollectionViewController

---
+++ NationalDex/ViewControllers/FavoritesCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesCollectionViewCell.swift
+//  NationalDex
+//
+//  Created by Kendall Poindexter on 5/18/20.
+//  Copyright © 2020 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var pokemonSprite: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    //May want to add prepare for reuse functionality
+}
